2f44145f77a84c8ccd30a2a52fedca6b245fce08
sharathpend <150897824+sharathpend@users.noreply.github.com>
2024-09-23 22:55:39 -0400

    Key Expansion Schedule

--- matlab/key_expansion_aes128.m
+++ matlab/key_expansion_aes128.m
@@ -1,18 +1,18 @@
 function expanded_keys = key_expansion_aes128(key, num_rows, Sbox_ram)
-    % https://www.brainkart.com/article/AES-Key-Expansion_8410/
+    % https://en.wikipedia.org/wiki/AES_key_schedule
+
     expanded_keys = initialize_round_keys(key, num_rows);
-    for i = [4:1:43]
-        temp_keys = expanded_keys(:, i);
+    rc = [01 02 04 08 hex2dec('10') hex2dec('20') hex2dec('40') hex2dec('80') hex2dec('1B') ...
+          hex2dec('36')];
+    N = 4;
+    num_rounds = 11;
+    
+    for i = 4:((4*num_rounds)-1)
+        temp_key = expanded_keys(:, i);
         if mod(i, 4) == 0
-            temp_keys = sub_bytes(circshift(temp_keys,-1), Sbox_ram);
-            n=1;
-            m=0;
-            while m < (i/4)-1
-                n = peasants_algorithm_mult(2,n);
-                m = m+1;
-            end
-            temp_keys = bitxor(temp_keys, [n,0,0,0]');
+            temp_key = bitxor(sub_bytes(circshift(temp_key,-1), Sbox_ram), [rc(i/N); 0; 0; 0]);
         end
-        expanded_keys(:, i+1) = bitxor(expanded_keys(:,(i-4)+1), temp_keys);
+        expanded_keys(:, (i+1)) = bitxor(temp_key, expanded_keys(:, (i-N)+1));
     end
+    
 end
