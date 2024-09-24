f9ccf17039065144d323ae8a39473ee365f75424
sharathpend <150897824+sharathpend@users.noreply.github.com>
2024-09-24 18:14:50 -0400

    Inverse Key Expansion fixed

--- matlab/inverse_key_expansion_aes128.m
+++ matlab/inverse_key_expansion_aes128.m
@@ -1,6 +1,6 @@
 function expanded_keys = inverse_key_expansion_aes128(key)
     % https://en.wikipedia.org/wiki/AES_key_schedule
-    Inverse_Sbox_ram = calc_inverse_sbox_matrix();
+    Sbox_ram = calc_sbox_matrix();
     N = 4;
     num_rounds = 11;
     rc = [1 2 4 8 16 32 64 128 27 54];
@@ -11,7 +11,7 @@ function expanded_keys = inverse_key_expansion_aes128(key)
     for i = ((4*num_rounds)-1):-1:4 % 43:-1:4
         temp_key = expanded_keys(:, i);
         if mod(i, 4) == 0
-            temp_key = bitxor(sub_bytes(circshift(temp_key,1), Inverse_Sbox_ram), [rc(i/N); 0; 0; 0]);
+            temp_key = bitxor(sub_bytes(circshift(temp_key,-1), Sbox_ram), [rc(i/N); 0; 0; 0]);
         end
         expanded_keys(:, (i-N+1)) = bitxor(temp_key, expanded_keys(:, (i+1)));
     end
