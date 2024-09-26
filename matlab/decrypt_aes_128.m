aab3345dde04b00ae17cc6b20374899d470972b1
sharathpend <150897824+sharathpend@users.noreply.github.com>
2024-09-26 09:18:31 -0400

    Updated decrypt function to use Key Expansion

--- matlab/decrypt_aes_128.m
+++ matlab/decrypt_aes_128.m
@@ -6,40 +6,49 @@
 % https://github.com/sharathpend/ECE592_Assignment1
 % You might have to request access if you want to see that repo.
 
-function plain_text = decrypt_aes_128(cipher_text, debug)
+function plain_text = decrypt_aes_128(cipher_text, key, debug)
     
-    if nargin > 2
+    if nargin > 3
         fprintf("Too many arguments.");
     elseif nargin < 1
         fprinf("Too few arguments.");
     elseif nargin == 1
+        key = '000102030405060708090a0b0c0d0e0f';
+        debug = 0;
+    elseif nargin == 2
         debug = 0;
     end
 
     % All round keys given in problem statement. Did not implement key
     % expansion function
-    round_key_1  = '000102030405060708090a0b0c0d0e0f';
-    round_key_2  = 'd6aa74fdd2af72fadaa678f1d6ab76fe';
-    round_key_3  = 'b692cf0b643dbdf1be9bc5006830b3fe';
-    round_key_4  = 'b6ff744ed2c2c9bf6c590cbf0469bf41';
-    round_key_5  = '47f7f7bc95353e03f96c32bcfd058dfd';
-    round_key_6  = '3caaa3e8a99f9deb50f3af57adf622aa';
-    round_key_7  = '5e390f7df7a69296a7553dc10aa31f6b';
-    round_key_8  = '14f9701ae35fe28c440adf4d4ea9c026';
-    round_key_9  = '47438735a41c65b9e016baf4aebf7ad2';
-    round_key_10 = '549932d1f08557681093ed9cbe2c974e';
-    round_key_11 = '13111d7fe3944a17f307a78b4d2b30c5';
+    %round_key_1  = '000102030405060708090a0b0c0d0e0f';
+    %round_key_2  = 'd6aa74fdd2af72fadaa678f1d6ab76fe';
+    %round_key_3  = 'b692cf0b643dbdf1be9bc5006830b3fe';
+    %round_key_4  = 'b6ff744ed2c2c9bf6c590cbf0469bf41';
+    %round_key_5  = '47f7f7bc95353e03f96c32bcfd058dfd';
+    %round_key_6  = '3caaa3e8a99f9deb50f3af57adf622aa';
+    %round_key_7  = '5e390f7df7a69296a7553dc10aa31f6b';
+    %round_key_8  = '14f9701ae35fe28c440adf4d4ea9c026';
+    %round_key_9  = '47438735a41c65b9e016baf4aebf7ad2';
+    %round_key_10 = '549932d1f08557681093ed9cbe2c974e';
+    %round_key_11 = '13111d7fe3944a17f307a78b4d2b30c5';
     num_rows = 4; % row size of state matrix for AES-128
     num_cols = 4;
     total_rounds = 11;
 
-    all_round_keys_hex = strcat(round_key_1, round_key_2, round_key_3, round_key_4, ...
-                                round_key_5, round_key_6, round_key_7, round_key_8, ...
-                                round_key_9, round_key_10, round_key_11);
-    round_keys_matrix = initialize_round_keys(all_round_keys_hex, num_rows);
+    %all_round_keys_hex = strcat(round_key_1, round_key_2, round_key_3, round_key_4, ...
+    %                            round_key_5, round_key_6, round_key_7, round_key_8, ...
+    %                            round_key_9, round_key_10, round_key_11);
+    %round_keys_matrix = initialize_round_keys(all_round_keys_hex, num_rows);
     
     % Calculate S-Box matrix one time.
     Inverse_Sbox_ram = calc_inverse_sbox_matrix();
+
+    % Calculate S-Box matrix one time.
+    Sbox_ram = calc_sbox_matrix();
+    
+    round_keys_matrix = key_expansion_aes128(key, num_rows, Sbox_ram);
+    
 
     % Take 2 characters at a time, automatically calculate missing
     % dimension. Convert that value from hext to dec. This results in a
